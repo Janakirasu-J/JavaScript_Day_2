a4fa6eccc48189497d1a215a6e92e9b8df6fd378
Janakirasu-J <johnrasu21@gmail.com>
2024-11-12 20:22:34 +0530

    commit

---
+++ script.js
@@ -0,0 +1,45 @@
+// console.log("Hello Would");
+
+//Variables Var Let Const;
+
+//Var
+var a = 10;
+console.log(a); //declare
+a = 20;
+console.log(a); //reassign
+var a = 30;
+console.log(a); //redeclare
+
+//Let
+
+let b = 100;
+console.log(b); //declare
+b = 200;
+console.log(b); //reassign
+
+//Const
+
+const c = 1000;
+console.log(c); //declare onley
+
+//Window Events ;
+// Alert
+
+// alert("Well Come My page")
+
+//confirm
+
+// confirm("Are You sure My Web Page Enter");
+
+//prompt Use Del
+// const del = ;
+// console.log(del);
+
+let person ={
+    name:"John",
+    age:29,
+    email:"Johnrasu21@gmail.com",
+    phn_num:88709736,
+
+}
+console.log(person)
